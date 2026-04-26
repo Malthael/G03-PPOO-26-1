5ea39b24943d939ef4b5138a97dc8107f4f34049
Andres <carlos.medina@softgic.co>
2026-04-26 11:00:13 -0500

    Se adiciona repository y conexiones

--- src/main/java/com/example/tuto002/models/Student.java
+++ src/main/java/com/example/tuto002/models/Student.java
@@ -1,6 +1,42 @@
 package com.example.tuto002.models;
 
-//@Entity
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estudiante")
 public class Student {
+    
+    @Id
+    private int id;
+    private String names;
+    private String lastNames;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNames() {
+        return this.names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getLastNames() {
+        return this.lastNames;
+    }
+
+    public void setLastNames(String lastNames) {
+        this.lastNames = lastNames;
+    }
+
+
 
 }
---
+++ src/main/java/com/example/tuto002/repositories/IStudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.tuto002.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.tuto002.models.Student;
+
+public interface IStudentRepository  extends CrudRepository<Student,Integer> {
+}
