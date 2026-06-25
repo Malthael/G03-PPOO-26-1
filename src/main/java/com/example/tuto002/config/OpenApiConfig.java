968890d0832f0eaf2467e773d126e6bdf015c0db
Malthael <diegoaburgos22@gmail.com>
2026-06-24 23:31:50 -0500

    se agrega el github del proyecto en la documentación

--- src/main/java/com/example/tuto002/config/OpenApiConfig.java
+++ src/main/java/com/example/tuto002/config/OpenApiConfig.java
@@ -2,8 +2,6 @@ package com.example.tuto002.config;
 
 //Clase de configuración
 
-
-
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,29 +13,25 @@ import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-        .info(new Info()
-                .title("Configuración de Api :)")
-                .version("1.1.13")
-                .description("Aplicación de POO I")
-                .contact(new Contact()
-                        .name("Diego Burgos")
-                        .email("daburgosr@ut.edu.co")
-                )
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Configuración de Api :)")
+                                                .version("1.1.13")
+                                                .description("Aplicación de POO I")
+                                                .contact(new Contact()
+                                                                .name("Diego Burgos")
+                                                                .email("daburgosr@ut.edu.co"))
 
+                                )
+                                // Info adicional
+                                .addServersItem(new Server()
+                                                .url("http://localhost:8080")
+                                                .description("Servidor Local de Desarrollo"))
+                                .externalDocs(new ExternalDocumentation()
+                                                .description("GitHub del Proyecto")
+                                                .url("https://github.com/Malthael/G03-PPOO-26-1"));
 
-        )
-                //Info adicional
-                .addServersItem(new Server()
-                        .url("http://localhost:8080")
-                        .description("Servidor Local de Desarrollo")
-                )
-                .externalDocs(new ExternalDocumentation()
-                        .description("Wiki del Proyecto de POO I (Aún no lo he subido al github XD")
-                        .url("https://github.com/Malthael/")
-                );
-
-    }
+        }
 }
